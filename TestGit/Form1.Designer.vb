287efafb826905afb05f47414b20793191366f37
Fabian Ramirez <fabianrm@gmail.com>
2015-02-06 23:25:11 -0500

    Formulario diseniado

--- TestGit/Form1.Designer.vb
+++ TestGit/Form1.Designer.vb
@@ -23,27 +23,62 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.TextEdit2 = New DevExpress.XtraEditors.TextEdit()
+        Me.DropDownButton1 = New DevExpress.XtraEditors.DropDownButton()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'SimpleButton1
         '
-        Me.SimpleButton1.Location = New System.Drawing.Point(163, 146)
+        Me.SimpleButton1.Location = New System.Drawing.Point(263, 233)
         Me.SimpleButton1.Name = "SimpleButton1"
         Me.SimpleButton1.Size = New System.Drawing.Size(75, 23)
         Me.SimpleButton1.TabIndex = 0
         Me.SimpleButton1.Text = "SimpleButton1"
+        '
+        'TextEdit1
+        '
+        Me.TextEdit1.Location = New System.Drawing.Point(36, 98)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Size = New System.Drawing.Size(302, 20)
+        Me.TextEdit1.TabIndex = 1
+        '
+        'TextEdit2
+        '
+        Me.TextEdit2.Location = New System.Drawing.Point(36, 137)
+        Me.TextEdit2.Name = "TextEdit2"
+        Me.TextEdit2.Size = New System.Drawing.Size(302, 20)
+        Me.TextEdit2.TabIndex = 2
+        '
+        'DropDownButton1
+        '
+        Me.DropDownButton1.Location = New System.Drawing.Point(38, 187)
+        Me.DropDownButton1.Name = "DropDownButton1"
+        Me.DropDownButton1.Size = New System.Drawing.Size(300, 23)
+        Me.DropDownButton1.TabIndex = 3
+        Me.DropDownButton1.Text = "DropDownButton1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(406, 306)
+        Me.Controls.Add(Me.DropDownButton1)
+        Me.Controls.Add(Me.TextEdit2)
+        Me.Controls.Add(Me.TextEdit1)
         Me.Controls.Add(Me.SimpleButton1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents TextEdit1 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TextEdit2 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents DropDownButton1 As DevExpress.XtraEditors.DropDownButton
 
 End Class
--- TestGit/Form1.vb
+++ TestGit/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
-        MessageBox.Show("Hola")
+        MessageBox.Show("Hola " + TextEdit1.Text)
+        MessageBox.Show("Gracias por venir")
     End Sub
 End Class
