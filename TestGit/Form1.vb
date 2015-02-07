610731aacce846c27cf85fffe29c4fed95e7086d
Fabian Ramirez <fabianrm@gmail.com>
2015-02-06 23:32:56 -0500

    Formulario diseniadosd

--- TestGit/Form1.vb
+++ TestGit/Form1.vb
@@ -1,8 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
-        MessageBox.Show("Hola " + TextEdit1.Text)
-        MessageBox.Show("Gracias por venir")
+        MessageBox.Show("Hola")
     End Sub
 
     Private Sub TextEdit1_EditValueChanged(sender As Object, e As EventArgs) Handles TextEdit1.EditValueChanged
